bc2303d913fd3d385696259b24f1c9d460de29bf
kunbpc <498031406@qq.com>
2024-12-31 21:22:52 +0800

    try to deploy the webpage
    Signed-off-by: kunbpc <498031406@qq.com>

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: env.VITE_BASE_PATH || '/webpage_test/',
+    base: env.VITE_BASE_PATH || '',
     resolve: {
       alias: {
         '~/': `${path.resolve(__dirname, 'src')}/`,
