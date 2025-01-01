4731e26e0c765de2646fc0fe2cfbff94191eebbd
kunbpc <498031406@qq.com>
2025-01-02 01:55:19 +0800

    nothing
    Signed-off-by: kunbpc <498031406@qq.com>

--- docs/assets/team-VN9w_QE4.js
+++ docs/assets/team-VN9w_QE4.js
@@ -1,4 +1,4 @@
-import{l as $,r as k,d as Z,q as p,v as w,x as t,C as e,z as i,M as h,B as A,A as W,Y as b,W as f,t as v,K as C,L as D,a0 as q,a1 as G}from"./index-p377Eczy.js";import{E as B,a as E}from"./anchor-link-DPJSLDA6.js";import{E as L,_ as j}from"./_plugin-vue_export-helper-DcnbPORZ.js";import"./raf-BGL5Rou_.js";const a=".",X=[{name:"郭裕兰",title:"教授",img:`/${a}/avatars/GuoYulan.jpeg`,area:"团队负责人",url:"https://www.yulanguo.cn/",email:"guoyulan@sysu.edu.cn"},{name:"黄小红",title:"副教授",img:"",area:`图像处理与识别
+import{l as $,r as k,d as Z,q as p,v as w,x as t,C as e,z as i,M as h,B as A,A as W,Y as b,W as f,t as v,K as C,L as D,a0 as q,a1 as G}from"./index-Dm4Eix7k.js";import{E as B,a as E}from"./anchor-link-BIsDumWA.js";import{E as L,_ as j}from"./_plugin-vue_export-helper-By1u9ujq.js";import"./raf-BUlOyaKU.js";const a=".",X=[{name:"郭裕兰",title:"教授",img:`/${a}/avatars/GuoYulan.jpeg`,area:"团队负责人",url:"https://www.yulanguo.cn/",email:"guoyulan@sysu.edu.cn"},{name:"黄小红",title:"副教授",img:"",area:`图像处理与识别
 
 获国家科技进步二等奖`,url:"",email:"huangxh75@mail.sysu.edu.cn"},{name:"张鹏",title:"副教授",img:`/${a}/avatars/ZhangPeng.jpeg`,area:`高分辨率对地观测
 
