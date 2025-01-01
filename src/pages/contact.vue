276bef4030d7cc64ac5a55576c58cd6de5e44477
kunbpc <498031406@qq.com>
2025-01-02 02:29:40 +0800

    contact.vue: 调整导航栏，调整内容 ResourceCategory.vue: 0.14*height -> 0.15*height
    Signed-off-by: kunbpc <498031406@qq.com>

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-
+const handleClick = (url: any) => {
+  window.open(url, '_blank')
+}
 </script>
 
 <template>
   <el-aside position="fixed" style="height: 100%; margin-top: 0px; ">
     <el-anchor :offset="70" style="height: 100%; text-align: left; background-color: #ffffff;">
+
+      <el-anchor-link href="#enrollment"><el-card class="shadow"><span class="link-text">研究生招生</span></el-card></el-anchor-link>
       <el-anchor-link href="#employment"><el-card class="shadow"><span class="link-text">博士后招聘</span></el-card></el-anchor-link>
-      <el-anchor-link href="#enrollment"><el-card class="shadow"><span class="link-text">招生</span></el-card></el-anchor-link>
-      <el-anchor-link href="#enrollment_phd"><el-card class="shadow"><span class="link-text">博士招生</span></el-card></el-anchor-link>
-      <el-anchor-link href="#enrollment_master"><el-card class="shadow"><span class="link-text">硕士招生</span></el-card></el-anchor-link>
       <el-anchor-link href="#enrollment_bachelor"><el-card class="shadow"><span class="link-text">本科生科研培训</span></el-card></el-anchor-link>
+      <el-anchor-link href="#enrollment_policy"><el-card class="shadow"><span class="link-text">相关政策参考</span></el-card></el-anchor-link>
     </el-anchor>
   </el-aside>
 
@@ -40,40 +42,16 @@
                 实验室不招收任何以混文凭或混经历为目的的成员。鼓励硕士生毕业后读本组博士或读海外一流研究组的博士，鼓励博士生毕业后在国内外一流高校或IT头部企业继续从事科研工作，鼓励学生参与国内外学术交流，并尽可能提供一切便利。
               </p>
               <p>
-			          希望你符合以下几点：
+                <div>希望你符合以下几点：</div>
+                <div class="text_item"><b>1. 较强的自我驱动力</b>：有明确的人生目标，了解自己内心的渴望，明白自己想成为一个什么样的人，懂得自律，并具备为了梦想持续刻苦努力的勇气和毅力。</div>
+                <div class="text_item"><b>2. 浓厚的研究兴趣</b>：希望通过做研究的过程，持续提升自己的逻辑思维能力、理性思考能力、前沿判断能力、算法设计能力及工程实践能力。</div>
+                <div class="text_item"><b>3. 良好的学习基础</b>：本科基础好（主要看重数学类、物理类和专业基础类课程），英语和专业类课程是额外加分项。</div>
               </p>
               <p>
-                1. 较强的自我驱动力
-                <br/>
-                有明确的人生目标，了解自己内心的渴望，明白自己想成为一个什么样的人，懂得自律，并具备为了梦想持续刻苦努力的勇气和毅力。
-              </p>
-              <p>
-                2. 浓厚的研究兴趣
-                <br/>
-                希望通过做研究的过程，持续提升自己的逻辑思维能力、理性思考能力、前沿判断能力、算法设计能力及工程实践能力。
-              </p>
-              <p>
-                3. 良好的学习基础
-                <br/>
-                本科基础好（主要看重数学类、物理类和专业基础类课程），英语和专业类课程是额外加分项。
-              </p>
-              <p>
-			          希望你在实验室内能够收获以下几点：
-              </p>
-              <p>
-                1. 扎实的算法功底
-                <br/>
-                学会如何找到学术前沿，如何发现科学问题并寻找解决方案，如何设计算法并编程实现，如何开展算法评估测试与迭代改进。
-              </p>
-              <p>
-                2. 良好的表达能力
-                <br/>
-                学会如何写一篇逻辑严密、论证充分、表达清晰的学术论文；学会如何做一个优雅得体、引人入胜的学术报告。
-              </p>
-              <p>
-                3. 开放的个人胸怀
-                <br/>
-                学会与团队的良好协作，与全球智力精英的思维碰撞与交流，与企业和学术界的交流与合作，掌握持续自我学习的能力。
+                <div>希望你在实验室内能够收获以下几点：</div>
+                <div class="text_item"><b>1. 扎实的算法功底</b>：学会如何找到学术前沿，如何发现科学问题并寻找解决方案，如何设计算法并编程实现，如何开展算法评估测试与迭代改进。</div>
+                <div class="text_item"><b>2. 良好的表达能力</b>：学会如何写一篇逻辑严密、论证充分、表达清晰的学术论文；学会如何做一个优雅得体、引人入胜的学术报告。</div>
+                <div class="text_item"><b>3. 开放的个人胸怀</b>：学会与团队的良好协作，与全球智力精英的思维碰撞与交流，与企业和学术界的交流与合作，掌握持续自我学习的能力。</div>
               </p>
             </div>
           </el-row>
@@ -92,9 +70,8 @@
                 实验室长期招聘博士后，可申报中山大学逸仙博士后项目，同时可享受广东及深圳相关博后补贴，待遇优厚。欢迎在人工智能、计算机、电子信息等相学科取得博士学位，致力于从事三维视觉、具身智能等相关研究，有良好的理论和实践基础，有刻苦钻研的精神和强烈的事业心，有良好的身体心理素质和团队合作精神的博士与我们联系。
               </p>
               <p>
-              相关招聘信息：https://rcb.sysu.edu.cn/article/547
-                <br/>
-              相关政策信息：https://rsc.sysu.edu.cn/postdoctor/article/128
+                <div>相关招聘信息：<el-link class="link_text" @click="handleClick('https://rcb.sysu.edu.cn/article/547')">https://rcb.sysu.edu.cn/article/547</el-link></div>
+                <div>相关政策信息：<el-link class="link_text" @click="handleClick('https://rsc.sysu.edu.cn/postdoctor/article/128')">https://rsc.sysu.edu.cn/postdoctor/article/128</el-link></div>
               </p>
             </div>
           </el-row>
@@ -113,16 +90,16 @@
             </div>
           </el-row>
 		  
-		  <el-row id="enrollment_bachelor">
+		  <el-row id="enrollment_policy">
             <div class="card-header">
               <div class="title_bar" />
               <span class="title_text">相关政策参考</span>
             </div>
             <div class="card-body">
               <p>
-			          中山大学博士研究生招生公告：https://graduate.sysu.edu.cn/zsw/doctor
-                <br/>
-                中山大学硕士研究生招生公告：https://graduate.sysu.edu.cn/zsw/postgraduate
+                <div>相关政策请关注中山大学研究生招生网：<el-link class="link_text" @click="handleClick('https://graduate.sysu.edu.cn/zsw/')">https://graduate.sysu.edu.cn/zsw/</el-link></div>
+                <div>中山大学博士研究生招生公告：<el-link class="link_text" @click="handleClick('https://graduate.sysu.edu.cn/zsw/doctor')">https://graduate.sysu.edu.cn/zsw/doctor</el-link></div>
+                <div>中山大学硕士研究生招生公告：<el-link class="link_text" @click="handleClick('https://graduate.sysu.edu.cn/zsw/postgraduate')">https://graduate.sysu.edu.cn/zsw/postgraduate</el-link></div>
               </p>
             </div>
           </el-row>
@@ -182,4 +159,12 @@
   text-indent: 2rem;
 }
 
+.text_item {
+  padding-left: 2rem;
+}
+
+.link_text {
+  font-size: 20px;
+  text-indent: 0rem;
+}
 </style>
