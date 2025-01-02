a182dc3de5c986152f2ed78a810abc29db32df68
Longguang Wang <wanglongguang01@163.com>
2025-01-02 11:36:40 +0800

    Update AreaInfo.ts

--- src/assets/data/Research/AreaInfo.ts
+++ src/assets/data/Research/AreaInfo.ts
@@ -161,26 +161,26 @@ export const AreatableData = computed<Area[]>(() => [
     {
         id: 'visial_sigmentation',
         area: '多模态融合与机器人',
-        intro: '致力于面向国家/地方重大需求，整合实验室在机器人多模态感知、理解、交互等方面的核心技术积累，探索具身机器人仿真、数据集构建、模型训练、Sim2Real迁移等技术，开展机器人遥操作、模仿学习、模型轻量化部署等研究，实现地面履带式/轮式/四足/双足机器人、空中无人机、水下无人系统的部署与应用。',
+        intro: '致力于面向国家/地方重大需求，整合实验室在机器人多模态感知、理解、交互、规划等方面的核心技术积累，探索具身机器人仿真、数据集构建、模型训练、Sim2Real迁移等技术，开展机器人遥操作、模仿学习、模型轻量化部署等研究，实现地面履带式/轮式/四足/双足机器人、空中无人机、水下无人系统的部署与应用。',
         img: `/${base}/research_area/visial_sigmentation.png`,
         papers: [       
             {
                 "title": "Parallel Dual-Hand Detection by Using Hand and Body Features for Robot Teleoperation",
-                "book": "IEEE Transactions on Human-Machine Systems (THMS),",
+                "book": "IEEE Transactions on Human-Machine Systems (THMS), 2023",
                 "author": "Qing Gao, Zhaojie Ju, Yongquan Chen, Qiwen Wang, Yinan Zhao, Shiwu Lai",
                 "url": "https://ieeexplore.ieee.org/abstract/document/10059173/",
                 "code": "",
             },
 			{
                 "title": "Exploring Fine-Grained Sparsity in Neural Networks for Efficient Inference",
-                "book": "IEEE Transactions on Pattern Analysis and Machine Intelligence (TPAMI)",
+                "book": "IEEE Transactions on Pattern Analysis and Machine Intelligence (TPAMI), 2023",
                 "author": "Longguang Wang, Yulan Guo, Xiaoyu Dong, Yingqian Wang, Xinyi Ying, Zaiping Lin, Wei An",
                 "url": "https://ieeexplore.ieee.org/abstract/document/9841044",
                 "code": "https://github.com/SYSU-SAIL/SMSR",
             },
             {
                 "title": "IF-ConvTransformer: A Framework for Human Activity Recognition using IMU Fusion and ConvTransformer",
-                "book": "Proceedings of the ACM on Interactive, Mobile, Wearable and Ubiquitous Technologies (IMWUT)",
+                "book": "Proceedings of the ACM on Interactive, Mobile, Wearable and Ubiquitous Technologies (IMWUT), 2022",
                 "author": "Ye Zhang, Yi Hou, Kewei Ouyang, Shilin Zhou, Yulan Guo",
                 "url": "https://dl.acm.org/doi/pdf/10.1145/3534584",
                 "code": "",
