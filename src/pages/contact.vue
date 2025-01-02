8c1beab9ced405adb4449cbc6338ab68bb290e54
Longguang Wang <wanglongguang01@163.com>
2025-01-02 09:57:58 +0800

    Update contact.vue

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -70,8 +70,7 @@ const handleClick = (url: any) => {
                 实验室长期招聘博士后，可申报中山大学逸仙博士后项目，同时可享受广东及深圳相关博后补贴，待遇优厚。欢迎在人工智能、计算机、电子信息等相学科取得博士学位，致力于从事三维视觉、具身智能等相关研究，有良好的理论和实践基础，有刻苦钻研的精神和强烈的事业心，有良好的身体心理素质和团队合作精神的博士与我们联系。
               </p>
               <p>
-                相关招聘信息：<el-link class="link_text" @click="handleClick('https://rcb.sysu.edu.cn/article/547')">https://rcb.sysu.edu.cn/article/547</el-link>
-		    
+                <div>相关招聘信息：<el-link class="link_text" @click="handleClick('https://rcb.sysu.edu.cn/article/547')">https://rcb.sysu.edu.cn/article/547</el-link></div>		    
                 <div>相关政策信息：<el-link class="link_text" @click="handleClick('https://rsc.sysu.edu.cn/postdoctor/article/128')">https://rsc.sysu.edu.cn/postdoctor/article/128</el-link></div>
               </p>
             </div>
@@ -99,8 +98,11 @@ const handleClick = (url: any) => {
             <div class="card-body">
               <p>
                 <div>相关政策请关注中山大学研究生招生网：<el-link class="link_text" @click="handleClick('https://graduate.sysu.edu.cn/zsw/')">https://graduate.sysu.edu.cn/zsw/</el-link></div>
+		    
                 <div>中山大学博士研究生招生公告：<el-link class="link_text" @click="handleClick('https://graduate.sysu.edu.cn/zsw/doctor')">https://graduate.sysu.edu.cn/zsw/doctor</el-link></div>
+		    
                 <div>中山大学硕士研究生招生公告：<el-link class="link_text" @click="handleClick('https://graduate.sysu.edu.cn/zsw/postgraduate')">https://graduate.sysu.edu.cn/zsw/postgraduate</el-link></div>
+		    
               </p>
             </div>
           </el-row>
